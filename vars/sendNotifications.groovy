196e6fbd612ebc82f30787a6dc692cf042885364
Matthias Fuchs <matthias.s.fuchs@gmail.com>
2019-01-03 18:51:07 +0100

    TRY OVERLOADING CALL FOR BACKWARDS COMPATIBILITY

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -3,6 +3,22 @@
 /**
  * Send a slack notifications if the build status has changed
  */
-def call(def buildStandalone = false)
+def call(boolean buildStandalone)
 {
+	echo "### Inside boolean function"
+	List channels = ['elem1']
+	if (buildStandalone) {
+		channels += 'elem2'
+	}
+}
+
+def call()
+{
+	echo "### Inside default function"
+	call(false)
+}
+
+def call(List channels)
+{
+	echo "### Inside channel function $channels"
 }
